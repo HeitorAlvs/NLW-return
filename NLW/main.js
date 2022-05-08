28e20e16bead205dfb353ee66ee95097807594a6
Heitor Alves <heitoralves.dev@gmail.com>
2022-05-07 22:10:41 -0300

    Section Services and About

--- NLW/main.js
+++ NLW/main.js
@@ -17,3 +17,27 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+/*CRIANDO UM OBJETO EM JS 
+    Function ScrollReveal(){
+      var options = {
+        reveal: function() {}
+      }
+      return options
+    }
+    ScrollReveal().reveal()*/
+/*Animações na página com ScrollReveal */
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 900,
+}).reveal(`
+    #home,
+    #home img,
+    #home .stats,
+    #services,
+    #services header,
+    #services .card
+    #about,
+    #about header,
+    #about .content`)
