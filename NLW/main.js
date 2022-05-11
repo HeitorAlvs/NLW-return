564cbbd4ebb8d70b3c141bb7ead1c2af20ec5736
Heitor Alves <heitoralves.dev@gmail.com>
2022-05-10 23:21:22 -0300

    Novas funções e melhorias no JavaScript

--- NLW/main.js
+++ NLW/main.js
@@ -1,19 +1,38 @@
+/* Add um evento na janela */
+window.addEventListener('scroll', onScroll)
+onScroll()
+
 /*alertas e mensagens
 alert(1)
 console.log("mensagem")*/
 /*Funções*/
 function onScroll() {
   /*Objetos e funcionalidades*/
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+/* Função para mostrar o navigation onscroll */
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
   }
 }
+/* Monstrar o botão back to top on scroll  */
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 600) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
 
+/*Abrir o menu */
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
+/* Fechar o menu */
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
@@ -30,7 +49,7 @@ function closeMenu() {
 ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 900,
+  duration: 900
 }).reveal(`
     #home,
     #home img,
