4bc2534388f769218602fd3e7d4a5ad0dee33060
Heitor Alves de Souza <heitor.meteorufcg@gmail.com>
2022-05-17 23:08:47 -0300

    Projeto Finalizado

--- NLW/main.js
+++ NLW/main.js
@@ -1,5 +1,6 @@
 /* Add um evento na janela */
 window.addEventListener('scroll', onScroll)
+
 onScroll()
 
 /*alertas e mensagens
@@ -10,6 +11,34 @@ function onScroll() {
   /*Objetos e funcionalidades*/
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 /* Função para mostrar o navigation onscroll */
 function showNavOnScroll() {
